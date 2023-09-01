67d9e3e05fa7a9245df943fab2541c0beffaad80
jmmarketing <jeff@jeffreymclean.com>
2023-09-01 12:11:41 -0700

    JS function for toggling Mobile Menu Overlay

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,1 +1,13 @@
 "use strict";
+
+const menu = document.querySelector("header");
+const mobileOverlay = document.querySelector(".mobile-menu-overlay");
+
+const toggleMobileMenu = function (e) {
+  if (e.target.classList.contains("fa-bars"))
+    mobileOverlay.classList.toggle("open");
+  if (e.target.classList.contains("fa-x"))
+    mobileOverlay.classList.toggle("open");
+};
+
+menu.addEventListener("click", toggleMobileMenu);
