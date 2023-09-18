5fd94b1d0ca27bd5aa59894b04d43c04b5255b0e
jmmarketing <jeff@jeffreymclean.com>
2023-09-18 11:49:57 -0700

    Start build of App Class. Skills cards will be rendered on load of class.

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,13 +1,24 @@
 "use strict";
 
+/* ###### DOM ELEMENTS ##########*/
 const menu = document.querySelector("header");
 const mobileOverlay = document.querySelector(".mobile-menu-overlay");
+const skillCardContainer = document.querySelector("#skills-card-container");
 
-const toggleMobileMenu = function (e) {
-  if (e.target.classList.contains("fa-bars"))
-    mobileOverlay.classList.toggle("open");
-  if (e.target.classList.contains("fa-x"))
-    mobileOverlay.classList.toggle("open");
-};
+/* ######### APP CLASS / INITIATOR ######### */
+class App {
+  #skills = [];
+  constructor() {
+    menu.addEventListener("click", this._toggleMobileMenu);
+  }
 
-menu.addEventListener("click", toggleMobileMenu);
+  _toggleMobileMenu(e) {
+    console.log(e);
+    if (e.target.classList.contains("fa-bars"))
+      mobileOverlay.classList.toggle("open");
+    if (e.target.classList.contains("fa-x"))
+      mobileOverlay.classList.toggle("open");
+  }
+}
+
+const app = new App();
