ca6896d67256905a3aa93268c79a042806effecc
jmmarketing <jeff@jeffreymclean.com>
2023-09-18 12:40:42 -0700

    Buildout of Class for Skills and Render Skills Method in App

--- resources/js/script.js
+++ resources/js/script.js
@@ -5,11 +5,28 @@ const menu = document.querySelector("header");
 const mobileOverlay = document.querySelector(".mobile-menu-overlay");
 const skillCardContainer = document.querySelector("#skills-card-container");
 
+let skillsArr = [];
+/*##### SKILLS & CASESTUDIES CLASSES ####### */
+class Skill {
+  constructor(name, exp, imgURL, type) {
+    this.name = name;
+    this.exp = exp;
+    this.img = imgURL;
+    this.type = [...type];
+    this._pushToArray();
+  }
+
+  _pushToArray() {
+    skillsArr.push(this);
+  }
+}
+
 /* ######### APP CLASS / INITIATOR ######### */
 class App {
   #skills = [];
   constructor() {
     menu.addEventListener("click", this._toggleMobileMenu);
+    this._renderSkillsCards(skillsArr);
   }
 
   _toggleMobileMenu(e) {
@@ -19,6 +36,32 @@ class App {
     if (e.target.classList.contains("fa-x"))
       mobileOverlay.classList.toggle("open");
   }
+
+  _renderSkillsCards(arr, type = "all") {
+    arr.forEach((skill) => {
+      const html = `
+      <div class="skill-card">
+        <div class="skill-icon-box">
+          <img src="${skill.img}" />
+        </div>
+        <div class="skill-text">
+          <p class="skill-card-title">${skill.name}</p>
+          <p class="skill-card-subtitle">${skill.exp} years experience</p>
+        </div>
+      </div>`;
+
+      skillCardContainer.insertAdjacentHTML("beforeend", html);
+    });
+  }
 }
+
+const skill_HTML5 = new Skill("HTML5", "5+", "./assets/images/html5.svg", [
+  "design-dev",
+  "technology",
+]);
+
+const skill_CSS = new Skill("CSS3", "5+", "./assets/images/html5.svg", [
+  "design-dev",
+]);
 
 const app = new App();
